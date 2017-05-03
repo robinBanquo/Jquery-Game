185403d7caa1ec817300d4c5a5723e05237b6e27
robinBanquo <caramaschi@hotmail.com>
2017-05-03 16:58:21 +0200

    animation buggé de la voiture rouge

--- script.js
+++ script.js
@@ -19,8 +19,28 @@ $(document).ready(function () {
         );
 
     }
-    $('#jeu').click(function () {
 
+    function redmove() {
+        $(window).keypress(function(e){
+            if(e.which == 37) {// flèche gauche
+                $("#vr").animate({"right": "-=5"},
+                    100,
+                    "linear"
+                );
+            }
+            if(e.which == 39) { // flèche droite
+                    $("#vr").animate({"right" : "+=5"},
+                        100 ,
+                        "linear"
+                    );
+            }
+        });
+
+
+    }
+
+    $('#start').click(function () {
+    redmove();
     defile();
     });
 
